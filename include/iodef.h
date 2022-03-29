ff5e250556b6f94d59faa1ae534a802c3ae11352
BONGJIN <jeong.luft@airsmed.com>
2022-03-30 05:06:26 +0900

    final work

--- include/iodef.h
+++ include/iodef.h
@@ -1,3 +1,39 @@
+const uint8_t MOTORS_LENGTH = 3;
 
+// TMC5160 SPI CS PIN
+#define mot0_CS 2
+#define mot1_CS 4
+#define mot2_CS 6
+
+// TMC5160 DRIVER ENABLE PIN
+#define mot0_DRV_ENN 3
+#define mot1_DRV_ENN 5
+#define mot2_DRV_ENN 7
+
+//LINEAR ACTUATOR SWITCH INPUT PIN
+#define lin0togglePin 33
+#define lin1pushPin 37
+#define lin2pushPin 41
+#define lin3pushPin 45
+#define lin4pushPin 49
+
+#define lin0pushPin 31
+#define lin1togglePin 35
+#define lin2togglePin 39
+#define lin3togglePin 43
+#define lin4togglePin 47
+
+//STEP MOTOR SWITCH PIN
+#define mot0DirSw 22
+#define mot1DirSw 24
+#define mot2DirSw 26
+
+#define mot0PwSw 28
+#define mot1PwSw 30
+#define mot2PwSw 32
+
+#define mot0Spd A0
+#define mot1Spd A1
+#define mot2Spd A2
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,14 @@
 #include <Arduino.h>
 #include <TMC5160.h>
-//#include "iodef.h"
+#include "iodef.h"
 #include "MightyZap.h"
 
 #define TMC_iRun 31
 #define TMC_iHold 20
 
-const uint8_t MOTORS_LENGTH = 3;
-
-// TMC5160 SPI CS PIN
-#define mot0_CS 2
-#define mot1_CS 4
-#define mot2_CS 6
-
-// TMC5160 DRIVER ENABLE PIN
-#define mot0_DRV_ENN 3
-#define mot1_DRV_ENN 5
-#define mot2_DRV_ENN 7
-
-//LINEAR ACTUATOR SWITCH INPUT PIN
-#define lin0togglePin 31
-#define lin1pushPin 37
-#define lin2pushPin 41
-#define lin3pushPin 45
-#define lin4pushPin 49
-
-#define lin0pushPin 33
-#define lin1togglePin 35
-#define lin2togglePin 39
-#define lin3togglePin 43
-#define lin4togglePin 47
-
-//STEP MOTOR SWITCH PIN
-#define mot0DirSw 22
-#define mot1DirSw 24
-#define mot2DirSw 26
-
-#define mot0PwSw 28
-#define mot1PwSw 30
-#define mot2PwSw 32
-
-#define mot0Spd A0
-#define mot1Spd A1
-#define mot2Spd A2
-
 int lin0ToggleSw = 0;
 int lin0pushSw = 0;
-int pressValue = 0;
+int pressValue = 1;
 
 bool mot0PushSw = 1;
 bool mot1PushSw = 1;
@@ -62,7 +24,7 @@ int conv_mot0AnalogVal = 0;
 int conv_mot1AnalogVal = 0;
 int conv_mot2AnalogVal = 0;
 
-MightyZap m_zap(&Serial2, 8);
+MightyZap m_zap(&Serial3, 10);
 
 TMC5160_SPI motors[MOTORS_LENGTH] =
         {
@@ -76,7 +38,7 @@ void setMotorBegin(TMC5160_SPI motors[])
     // This sets the motor & driver parameters /!\ run the configWizard for your driver and motor for fine-tuning !
     TMC5160::PowerStageParameters powerStageParams; // defaults.
     TMC5160::MotorParameters motorParams;
-    motorParams.globalScaler = 0; // Adapt to your driver and motor (check TMC5160 datasheet - "Selecting sense resistors")
+    motorParams.globalScaler = 98; // Adapt to your driver and motor (check TMC5160 datasheet - "Selecting sense resistors")
     motorParams.irun = TMC_iRun;
     motorParams.ihold = TMC_iHold;
     for (int i = 0; i < MOTORS_LENGTH; ++i)
@@ -90,7 +52,7 @@ void setMotorRamp(TMC5160_SPI motors[])
     for (int i = 0; i < MOTORS_LENGTH; ++i)
     {
         motors[i].setRampMode(TMC5160::VELOCITY_MODE);
-        motors[i].setAcceleration(500);
+        motors[i].setAcceleration(150);
     }
 }
 
@@ -107,8 +69,8 @@ void defaultState()
 
 void linActuator()
 {
-    lin0ToggleSw = digitalRead(lin0togglePin);
     lin0pushSw = digitalRead(lin0pushPin);
+    lin0ToggleSw = digitalRead(lin0togglePin);
 
     if (lin0pushSw == 0)
     {
@@ -116,14 +78,20 @@ void linActuator()
         {
             m_zap.GoalPosition(0, pressValue);
             m_zap.ledOn(0, RED);
-            delay(100);
+            delay(1000);
+            //Serial.println("actH");
             if (pressValue != 4096) pressValue++;
         } else
         {
             m_zap.GoalPosition(0, pressValue);
             m_zap.ledOn(0, GREEN);
+            delay(1000);
+            //Serial.println("actL");
             if (pressValue != 0) pressValue--;
         }
+    } else
+    {
+        delay(1);
     }
 }
 
@@ -140,15 +108,15 @@ void analogRead_Calculate()
 
 void stepMotorDrive()
 {
-//    mot0ToggleSw = digitalRead(mot0DirSw);
-//    mot1ToggleSw = digitalRead(mot1DirSw);
-//    mot2ToggleSw = digitalRead(mot2DirSw);
-
     mot0PushSw = digitalRead(mot0PwSw);
     mot1PushSw = digitalRead(mot1PwSw);
     mot2PushSw = digitalRead(mot2PwSw);
-    delay(100);
-    Serial.println(mot0PushSw);
+
+    mot0ToggleSw = digitalRead(mot0DirSw);
+    mot1ToggleSw = digitalRead(mot1DirSw);
+    mot2ToggleSw = digitalRead(mot2DirSw);
+
+    delay(10);
 
     if (mot0PushSw == 0)
     {
@@ -156,27 +124,55 @@ void stepMotorDrive()
         if (mot0ToggleSw == 1)
         {
             //Serial.println(mot0PushSw);
-            for (int i = 0; i < MOTORS_LENGTH; ++i)
-            {
-                motors[i].setMaxSpeed(300);
-            }
+            motors[0].setMaxSpeed(500);
         } else
         {
-            for (int i = 0; i < MOTORS_LENGTH; ++i)
-            {
-                motors[i].setMaxSpeed(-300);
-            }
+            motors[0].setMaxSpeed(-500);
+        }
+    } else
+    {
+        {
+            motors[0].stop();
         }
     }
-    else
+
+    if (mot1PushSw == 0)
     {
-        Serial.println("stop loop");
-        for (int i = 0; i < MOTORS_LENGTH; ++i)
+        Serial.println("loop in");
+        if (mot1ToggleSw == 1)
         {
-            motors[i].setMaxSpeed(0);
+            //Serial.println(mot0PushSw);
+            motors[1].setMaxSpeed(-500);
+        } else
+        {
+            motors[1].setMaxSpeed(500);
+        }
+    } else
+    {
+        {
+            motors[1].stop();
+        }
+    }
+
+    if (mot2PushSw == 0)
+    {
+        Serial.println("loop in");
+        if (mot2ToggleSw == 1)
+        {
+            //Serial.println(mot0PushSw);
+            motors[2].setMaxSpeed(500);
+        } else
+        {
+            motors[2].setMaxSpeed(-500);
+        }
+    } else
+    {
+        {
+            motors[2].stop();
         }
     }
 }
+
 
 void setup()
 {
@@ -187,9 +183,9 @@ void setup()
     pinMode(lin0togglePin, INPUT_PULLUP);
     pinMode(lin0pushPin, INPUT_PULLUP);
 
-    pinMode(mot0PushSw, INPUT_PULLUP);
-    pinMode(mot1PushSw, INPUT_PULLUP);
-    pinMode(mot2PushSw, INPUT_PULLUP);
+    pinMode(mot0PwSw, INPUT_PULLUP);
+    pinMode(mot1PwSw, INPUT_PULLUP);
+    pinMode(mot2PwSw, INPUT_PULLUP);
 
     pinMode(mot0ToggleSw, INPUT_PULLUP);
     pinMode(mot1ToggleSw, INPUT_PULLUP);
@@ -208,6 +204,6 @@ void setup()
 
 void loop()
 {
-    //linActuator();
+    linActuator();
     stepMotorDrive();
 }
